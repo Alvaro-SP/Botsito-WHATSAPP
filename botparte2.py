4465bd75e2804422ac63a5b89db98d657d58408f
Alvaro-SP <socop2412@gmail.com>
2022-05-25 11:51:29 -0600

    commit

--- botparte2.py
+++ botparte2.py
@@ -4,9 +4,9 @@ webbrowser.open("https://web.whatsapp.com/send?phone=+55123456789")
 sleep(10)
 
 with open ("spam.txt", "r") as file:
-for line in file:
-pyautogui.typewrite(line)
-pyautogui.press("enter")
+    for line in file:
+        pyautogui.typewrite(line)
+        pyautogui.press("enter")
 
 import pyautogui, webbrowser
 from time import sleep
--- wspbot.py
+++ wspbot.py
@@ -1,8 +1,9 @@
+import pyautogui, webbrowser
+from time import sleep
+
 webbrowser.open("https://web.whatsapp.com/send?phone=+50241137722&text=Hola%20como%20estas?enviadoTextoPorMiBotsito")
 sleep(10)
 
 pyautogui.typewrite("Hola estoy es un mensaje automatico")
 pyautogui.press("enter")
 
-import pyautogui, webbrowser
-from time import sleep
