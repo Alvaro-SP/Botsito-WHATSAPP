63c7725256b557db68ed755864524b908f8eba6d
Alvaro-SP <socop2412@gmail.com>
2022-06-02 22:09:35 -0600

    commit

--- commit.bat
+++ commit.bat
@@ -2,7 +2,9 @@
 @REM cd "C:\Users\dell\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5 Q U I N T O S E MESTRE\LAB. Compi 1\PROYECTO 2\GITHUB\-OLC1-Proyecto2_202000194"
 
 @REM PC DESKTOP
-cd "C:\Users\spuser\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\0. PROYECTOS EXTRA\Botsito-WHATSAPP"
+@REM cd "C:\Users\spuser\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\0. PROYECTOS EXTRA\Botsito-WHATSAPP"
+@echo off
+echo %~dp0
 git add .
 git commit -m "commit"
 git push
--- wspbot.py
+++ wspbot.py
@@ -1,9 +1,96 @@
 import pyautogui, webbrowser
 from time import sleep
 
-webbrowser.open("https://web.whatsapp.com/send?phone=+50241137722&text=Hola%20como%20estas?enviadoTextoPorMiBotsito")
+# webbrowser.open("https://web.whatsapp.com/send?phone=+50241137722&text=Hola%20como%20estas?enviadoTextoPorMiBotsito")
 sleep(10)
 
-pyautogui.typewrite("Hola estoy es un mensaje automatico")
-pyautogui.press("enter")
+for i in range(1,10):
+    pyautogui.typewrite("Hola estoy es un mensaje automatico")
+    pyautogui.press("enter")
+
+# 🤖soy el botsito: 🤖
+# '1 TEORÍA BÁSICA DE GESTIÓN DE RIESGO' 
+# Conferencia Virtual 01/06/22
+# 10:00am - 12:00pm
+# Ing. Silvia Ramírez
+# MEET: https://forms.gle/9obynKRK5E7Z5rsC8
+
+# Ultima Fecha para Entrega de Actividad 03/06/22 23:59pm 1
+
+
+
+# 🤖soy el botsito: 🤖
+# '2 MARCO LEGAL DE LA GESTIÓN DEL RIESGO DE
+# DESASTRES EN GUATEMALA' 
+# 03/06/22
+# 12:00pm - 02:00pm
+# Ing. Raúl Gabriel Vargas
+
+# Ultima Fecha para Entrega de Actividad 05/06/22 23:59pm 1
+
+
+
+# 🤖soy el botsito: 🤖
+# "3 COMPRENSIÓN DEL RIESGO SÍSMICO "
+# Conferencia Virtual 08/06/22
+# 10:00am - 12:00pm
+# Ing. Raúl Gabriel Vargas
+
+# Ultima Fecha para Entrega de Actividad 10/06/22 23:59pm 1
+
+
+
+
+
+# 🤖soy el botsito: 🤖
+# "4 TEORÍA BÁSICA DE AMENAZAS HIDROMETEOROLÓGICAS Y GEOLÓGICAS EN
+# GUATEMALA "
+# Conferencia Virtual 10/06/22
+# 10:00am - 12:00pm
+# Ing. Kevin Nufio
+
+# Ultima Fecha para Entrega de Actividad 12/06/22 23:59pm 1
+
+
+
+
+# 🤖soy el botsito: 🤖
+# "5 COMUNICACIÓN PARA LA GESTION DEL RIESGO
+# DE DESASTRES"
+# Conferencia Virtual 15/06/22
+# 10:00am - 12:00pm
+# Lic. Hugo Arbizú
+
+# Ultima Fecha para Entrega de Actividad 17/06/22 23:59pm
+
+
+
+
+# 🤖soy el botsito: 🤖
+# "6 FUNCIONAMIENTO CEDESYD"
+# Conferencia Virtual 17/06/22
+# 10:00am - 12:00pm
+# Ing. Kevin Nufio
+
+# Ultima Fecha para Entrega de Actividad 19/06/22 23:59pm
+
+
+
+# 🤖soy el botsito: 🤖
+# "7 LIDERAZGO PARA LA REDUCCION DE RIESGOS"
+# Conferencia Virtual 22/06/22
+# 10:00am - 12:00pm
+# Ing. Silvia Ramírez
+
+# Ultima Fecha para Entrega de Actividad 19/06/22 23:59pm
+
+
+
+# 🤖soy el botsito: 🤖
+# "8 PRIMEROS AUXILIOS PSICOLÓGICOS"
+# Conferencia Virtual 24/06/22
+# 10:00am - 12:00pm
+# Licda. Marysabel 
+
+# Ultima Fecha para Entrega de Actividad 26/06/22 23:59pm
 
